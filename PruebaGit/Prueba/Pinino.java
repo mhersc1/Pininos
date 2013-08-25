67b811ccab2edff0b3def16534e6f542e3de47df
MHER <>
2013-08-24 19:29:48 -0500

    Solucionado #1, omar zela ... x)

---
+++ PruebaGit/Prueba/Pinino.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Prueba;
+
+/**
+ *
+ * @author MAYCOL
+ */
+public class Pinino {
+    String objetivo;
+    String importancia;
+    
+    public Pinino(String objetivo,String importancia){
+    this.objetivo=objetivo;
+    this.importancia=importancia;
+    }
+}
